e957700f8efc583539f44f57ed8e529bb59e0c44
darkimpow <darkimpowell@gmail.com>
2023-04-17 18:12:04 -0400

    updated styling for webpages and adding reactive settings with tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import Socials from "./Socials";
+import Toy from "../img/header/Toy.png";
+import MobileNav from "./MobileNav";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <div>Header</div>;
+  return <header className='bg-orange-200 fixed w-full px-[30px]
+  lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
+    <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
+      {/*logo placed here*/}
+    <Link to={'/'}>
+      {/*<img src={} alt='picture' />*/}
+    </Link>
+      {/*nav intially hidden - show onm desktop mode here*/}
+    </div>
+  </header>;
 };
 
 export default Header;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return <section className='section bg-orange-400'>About</section>;
 };
 
 export default About;
