5b13001271c929ed3f55972ae073145d2f00739e
darkimpow <darkimpowell@gmail.com>
2023-04-17 15:42:37 -0400

    Imported files for router dom

--- src/components/AnimRoutes.js
+++ src/components/AnimRoutes.js
@@ -1,5 +1,8 @@
 import React from 'react';
-
+import Home from '../pages/Home';
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Portfolio from "../pages/Portfolio";
 const AnimRoutes = () => {
   return <div>AnimRoutes</div>;
 };
