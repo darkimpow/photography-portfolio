ea3ffd867fc886e9ae580fad216053b367e7dc4c
darkimpow <darkimpowell@gmail.com>
2023-04-17 18:58:54 -0400

    custom side bar menu for mobile view completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,20 @@ const Header = () => {
   lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center'>
     <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
       {/*logo placed here*/}
-    <Link to={'/'}>
+    <Link to={'/'} className='max-w-[200px]'>
       {/*<img src={} alt='picture' />*/}
     </Link>
-      {/*nav intially hidden - show onm desktop mode here*/}
+      {/*nav initially hidden - show onm desktop mode here*/}
+      <nav className='hidden lg:flex gap-x-12 font-semibold  '>
+        <Link to={'/'} className='text-[#696c6d] hover:text-primary transition'>Home</Link>
+        <Link to={'/about'} className='text-[#696c6d] hover:text-primary transition'>About</Link>
+        <Link to={'/portfolio'} className='text-[#696c6d] hover:text-primary transition'>Portfolio</Link>
+        <Link to={'/contacts'} className='text-[#696c6d] hover:text-primary transition'>Contacts</Link>
+      </nav>
+      {/*socials links here*/}
+      <Socials />
+      {/*Mobile view*/}
+      <MobileNav />
     </div>
   </header>;
 };
