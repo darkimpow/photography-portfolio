da9dc22e9f073904088bc55f4fd946fe6b0061b8
darkimpow <darkimpowell@gmail.com>
2023-04-17 19:54:50 -0400

    client picture resized to the correct spec for fitting of web page 1037x761

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,29 @@
 import React from 'react';
-
+import {
+  ImFacebook,
+  ImTwitter,
+  ImInstagram,
+} from 'react-icons/im'
 const Socials = () => {
-  return <div className='hidden xl:flex ml-24'>Socials</div>;
+  return <div className='hidden xl:flex ml-24'>
+    <ul className='flex gap-x-4 '>
+      <li>
+        <a href="" target='_blank'>
+          <ImFacebook />
+        </a>
+      </li>
+      <li>
+        <a href="" target='_blank'>
+          <ImTwitter />
+        </a>
+      </li>
+      <li>
+        <a href="" target='_blank'>
+          <ImInstagram />
+        </a>
+      </li>
+    </ul>
+  </div>;
 };
 
 export default Socials;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,33 @@
 import React from 'react';
-
+import NuaNew from '../img/home/NuaNew.jpg'
+import { Link } from 'react-router-dom'
 const Home = () => {
-  return <section className='section bg-gray-200'>Home</section>;
+  return (
+      <section className='section'>
+        <div className="container mx-auto h-full relative">
+          {/* text & img wrapper */}
+          <div className='flex flex-col justify-center'>
+            <div>
+              {/* text */}
+              <div className='w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start '>
+                <h1 className='h1'>
+                  photographer <br /> & film maker
+                </h1>
+                <p className='text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12'>Boston, Ma</p>
+                <Link to='/contact' className='btn mb-[30px] '>Contact</Link>
+              </div>
+              {/* image here */}
+              <div className= 'flex justify-end  lg:max-h-max'>
+                <div className='relative lg:-right-40 overflow-hidden'>
+                  <img src={NuaNew} alt='picture' />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+  );
 };
+
 
 export default Home;
