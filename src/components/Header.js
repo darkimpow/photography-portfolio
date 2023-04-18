f0db9cd8b5e3d2e725557f26469e3d4f26039d62
darkimpow <darkimpowell@gmail.com>
2023-04-18 12:07:19 -0400

    completed transitions with outline of portfolio pictures for grid effect, will change soon for a different effect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Socials from "./Socials";
-import Toy from "../img/header/Toy.png";
+import NuaLogo from "../img/header/NuaLogo.png";
 import MobileNav from "./MobileNav";
 import { Link } from 'react-router-dom';
 
@@ -10,15 +10,17 @@ const Header = () => {
     <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
       {/*logo placed here*/}
     <Link to={'/'} className='max-w-[200px]'>
-      {/*<img src={} alt='picture' />*/}
+      <img src={NuaLogo} alt='picture' />
     </Link>
       {/*nav initially hidden - show onm desktop mode here*/}
-      <nav className='hidden lg:flex gap-x-12 font-semibold  '>
+    <div>
+      <nav className='hidden lg:flex gap-x-12 font-semibold pl-[800px]  '>
         <Link to={'/'} className='text-[#696c6d] hover:text-primary transition'>Home</Link>
         <Link to={'/about'} className='text-[#696c6d] hover:text-primary transition'>About</Link>
         <Link to={'/portfolio'} className='text-[#696c6d] hover:text-primary transition'>Portfolio</Link>
         <Link to={'/contact'} className='text-[#696c6d] hover:text-primary transition'>Contact</Link>
       </nav>
+    </div>
       {/*socials links here*/}
       <Socials />
       {/*Mobile view*/}
